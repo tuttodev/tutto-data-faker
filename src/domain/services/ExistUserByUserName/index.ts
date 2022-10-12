130e360fc50d89f423b859a611fa67c056f13e2a
estebanjosedemarchi@gmail.com <estebanjosedemarchi@gmail.com>
2022-10-11 21:00:56 -0300

    Fix typo in variable name

--- src/domain/services/ExistUserByUserName/index.ts
+++ src/domain/services/ExistUserByUserName/index.ts
@@ -1,14 +1,14 @@
 import { UserRepository } from 'domain/repositories/UserRepository'
 
 export class ExistUserByUserName {
-  private readonly _userResposiory: UserRepository
+  private readonly _userRepository: UserRepository
 
   constructor (userRepository: UserRepository) {
-    this._userResposiory = userRepository
+    this._userRepository = userRepository
   }
 
   async run (username: string): Promise<boolean> {
-    const user = await this._userResposiory.getByUserName(username)
+    const user = await this._userRepository.getByUserName(username)
 
     if (user !== null) return true
 
--- src/domain/services/UserGetterById/index.ts
+++ src/domain/services/UserGetterById/index.ts
@@ -3,14 +3,14 @@ import { UserRepository } from 'domain/repositories/UserRepository'
 import { UserNotFoundException } from '../../exceptions/UserNotFoundException'
 
 export class UserGetterById {
-  private readonly _userResposiory: UserRepository
+  private readonly _userRepository: UserRepository
 
   constructor (userRepository: UserRepository) {
-    this._userResposiory = userRepository
+    this._userRepository = userRepository
   }
 
   async run (id: string): Promise<User> {
-    const user = await this._userResposiory.getById(id)
+    const user = await this._userRepository.getById(id)
 
     if (user === null) { throw new UserNotFoundException() }
 
